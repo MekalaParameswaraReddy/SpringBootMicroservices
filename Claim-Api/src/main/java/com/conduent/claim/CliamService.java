bf94094930ec6a43a7afea472c589d34bcc94bb2
MekalaParameswaraReddy <parameswarareddy2@gmail.com>
2019-12-01 21:42:47 +0530

    Un nessary imports [RequestMapping, RequestMethod] are organized.

--- Claim-Api/src/main/java/com/conduent/claim/CliamService.java
+++ Claim-Api/src/main/java/com/conduent/claim/CliamService.java
@@ -9,8 +9,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
 import com.conduent.claim.bean.Claim;
