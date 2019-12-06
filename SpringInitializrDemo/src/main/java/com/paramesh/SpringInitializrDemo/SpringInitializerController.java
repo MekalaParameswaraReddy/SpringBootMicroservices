3ce9e3d257637f59ad944ecf9d19a49823b85631
MekalaParameswaraReddy <parameswarareddy2@gmail.com>
2019-12-06 16:23:43 +0530

    initial commit

---
+++ SpringInitializrDemo/src/main/java/com/paramesh/SpringInitializrDemo/SpringInitializerController.java
@@ -0,0 +1,19 @@
+package com.paramesh.SpringInitializrDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author: Paramesh
+ * @version:
+ * @Since:
+ */
+@RestController
+public class SpringInitializerController {
+		
+	@RequestMapping("/springInitializer")
+	public String sayHello(){
+		return "This intialized from Spring Initializer ... !!";
+	}	
+}
---
+++ SpringInitializrDemo/src/test/java/com/paramesh/SpringInitializrDemo/SpringInitializrDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paramesh.SpringInitializrDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringInitializrDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
