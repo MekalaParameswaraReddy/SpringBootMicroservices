fc5802e66162803a8ff709d10c35c032ce6132b4
MekalaParameswaraReddy <parameswarareddy2@gmail.com>
2021-03-23 18:30:12 +0530

    YAML config support added.

--- provider-api/src/main/java/com/conduent/provider/ProviderApp.java
+++ provider-api/src/main/java/com/conduent/provider/ProviderApp.java
@@ -1,7 +1,11 @@
 package com.conduent.provider;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.conduent.provider.config.YAMLConfig;
 
 /**
  * @author: Paramesh
@@ -9,13 +13,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Since:
  */
 @SpringBootApplication
-public class ProviderApp {
-
+public class ProviderApp implements CommandLineRunner {
+	
+	@Autowired
+	private YAMLConfig yAMLConfig;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {		
-		SpringApplication.run(ProviderApp.class, args);
+		SpringApplication app = new SpringApplication(ProviderApp.class);
+        app.run();
+	}
+	
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println(yAMLConfig.getEnvironment());
+		System.out.println(yAMLConfig.getName());
+		System.out.println(yAMLConfig.getServers());
+		System.out.println(yAMLConfig.isEnabled());
+		
 	}
 
 }
